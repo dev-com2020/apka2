f4f6b6240974af62d30903d391a5ea249c65d6f3
dev-com2020 <kwp@tlen.pl>
2023-10-19 15:15:57 +0200

    Initial commit

---
+++ src/main/java/com/example/apka2/Apka2Application.java
@@ -0,0 +1,13 @@
+package com.example.apka2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apka2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Apka2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apka2/Apka2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apka2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Apka2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
